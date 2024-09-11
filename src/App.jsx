8ed1654ab6980ed3e7256e86603579508896e2ff
wacho91 <criskol.71@gmail.com>
2024-09-11 11:32:21 -0500

    Navabar Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
+import Navbar from "./Component/NavBar/Navbar"
 
 
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Navbar />
   )
 }
 
