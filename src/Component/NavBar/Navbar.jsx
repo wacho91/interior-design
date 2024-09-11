4bc389069b27ed6b02c383e92cc29ed5eb13774e
wacho91 <criskol.71@gmail.com>
2024-09-11 11:52:33 -0500

    Navbar Construction

--- src/Component/NavBar/Navbar.jsx
+++ src/Component/NavBar/Navbar.jsx
@@ -1,12 +1,53 @@
+import Logo from "../../assets/logo.png"
 
+const NavLinks = [
+    {
+        id: 1,
+        title: "About",
+        link: "#"
+    },
+
+    {
+        id: 2,
+        title: "Services",
+        link: "#",
+      
+    },
+
+    {
+        id: 3,
+        title: "Project",
+        link: "#",
+    },
+
+    {
+        id: 4,
+        title: "Contact",
+        link: "#",
+    },
+]
 
 const Navbar = () => {
   return (
-    <div className="container">
+    <div className="container py-4 flex justify-between">
         {/*Logo Section*/}
-        
+        <img src={Logo} alt="Logo" className="w-12"/>
         {/*Link Section*/}
+        <div>
+            {
+                NavLinks.map((link) => {
+                    return (
+                        <a key={link.id} href={link.link} className="mx-4">
+                            {link.title}
+                        </a>
+                    )
+                })
+            }
+        </div>
         {/*Button Section*/}
+        <div>
+            <button className="">Try For Free</button>
+        </div>
     </div>
   )
 }
