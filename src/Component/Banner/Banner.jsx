a61a3f8a583687f1755f106dc0f2e6be82474e6a
wacho91 <criskol.71@gmail.com>
2024-09-13 10:22:34 -0500

    Banner Complete

--- src/Component/Banner/Banner.jsx
+++ src/Component/Banner/Banner.jsx
@@ -8,13 +8,16 @@ const Banner = () => {
             <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
                 {/*Image Section*/}
                 <div className="flex flex-col justify-center items-center">
-                    <img src={Banner1} alt="" />
+                    <img src={Banner1} alt=""  className="w-[95%] md:w-full mx-auto"/>
                 </div>
                 {/*Text Section*/}
-                <div className="space-y-3 flex justify-center flex-col">
+                <div className="space-y-5 flex justify-center flex-col">
                     <h1 className="text-5xl font-bold">We Believe that a team makes any project better</h1>
                     <p className="text-gray-500 text-sm"> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit
                     earum accusantium tempore nam aliquid{" "}</p>
+                    <div>
+                    <button className="primary-btn">Discover Now</button>
+                    </div>
                 </div>
             </div>
         </div>
