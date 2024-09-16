02dd19e511a9bae92e959386c04ba0ebc1289353
wacho91 <criskol.71@gmail.com>
2024-09-16 10:55:03 -0500

    Last change

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -6,7 +6,7 @@ import { LuMessageSquare } from "react-icons/lu";
 const Footer = () => {
   return (
     <div className="container py-11">
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-24">
             {/*Company Information*/}
             <div className="space-y-4 font-semibold">
                 <div className="flex items-center space-x-3">
@@ -17,7 +17,7 @@ const Footer = () => {
                 <p>@ 2024 Interior All rights reserved</p>
             </div>
             {/*Footer Links*/}
-            <div className="grid grid-cols-2 gap-3">
+            <div className="grid grid-cols-2 gap-16">
                 <div className="space-y-4">
                     <h1 className="text-xl font-semibold">About Us</h1>
                     <ul className="text-sm space-y-4">
