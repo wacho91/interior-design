86c8c8a52fcbf47e50b7f26c684f24c9be081fc8
wacho91 <criskol.71@gmail.com>
2024-09-16 10:44:33 -0500

    Footer Contact Complete

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -1,10 +1,12 @@
 
 import Logo from "../../assets/logo.png"
+import { FaPhone } from "react-icons/fa6";
+import { LuMessageSquare } from "react-icons/lu";
 
 const Footer = () => {
   return (
     <div className="container py-11">
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
             {/*Company Information*/}
             <div className="space-y-4 font-semibold">
                 <div className="flex items-center space-x-3">
@@ -54,17 +56,22 @@ const Footer = () => {
             {/*Contact Section*/}
             <div className="space-y-4">
                 <h1 className="text-xl font-semibold">Contact Us</h1>
-                <ul className="text-sm space-y-4">
-                    <li>
-                        <a href="#">FAQ'S</a>
+                <ul className="text-base space-y-4 font-semibold">
+                    <li className="flex items-center space-x-3">
+                        <FaPhone />
+                        <a href="#">+91 123456789</a>
                     </li>
-                    <li>
-                        <a href="#">Shipping & Retuens</a>
+                    <li className="flex items-center space-x-3">
+                        <LuMessageSquare />
+                        <a href="mailto:contact.thecodingjourney@gmail.com">Email</a>
                     </li>
-                    
                 </ul>
             </div>
       </div>
+      {/* bottom section */}
+      <p className="text-center text-sm font-semibold  border-t-2 pt-5 mt-5">
+          &copy; 2024 CD. All rights reserved
+      </p>
     </div>
   )
 }
