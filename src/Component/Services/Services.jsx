6316184d460a5e8d09790f328ee4ec183e305d37
wacho91 <criskol.71@gmail.com>
2024-09-17 10:49:21 -0500

    Animation services

--- src/Component/Services/Services.jsx
+++ src/Component/Services/Services.jsx
@@ -2,6 +2,8 @@
 import { BiSolidDollarCircle } from "react-icons/bi"
 import { FaVectorSquare } from "react-icons/fa"
 import { FaPenToSquare } from "react-icons/fa6"
+import { motion } from "framer-motion";
+import { SlideUp } from "../../Animation/Animate";
 
 
 
@@ -43,26 +45,41 @@ const Services = () => {
         <div className="container py-20">
             {/*Heading Section*/}
             <div className="space-y-2 text-center max-w-[350px] mx-auto mb-8">
-                <h1 className="text-3xl font-bold font-serif">
+                <motion.h1
+                    variants={SlideUp(0.2)}
+                    initial="initial"
+                    whileInView={"animate"}
+                    className="text-3xl font-bold font-serif"
+                >
                     What The Provide
-                </h1>
-                <p className="text-gray-500 text-sm">
+                </motion.h1>
+                <motion.p
+                    variants={SlideUp(0.4)}
+                    initial="initial"
+                    whileInView={"animate"}
+                    className="text-gray-500 text-sm"
+                >
                     Bring your dream home to life with one-on-one design help & picked products
-                </p>
+                </motion.p>
             </div>
             <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
                 {/*Card Section*/}
                 {
                     ServiceCard.map((card) => {
                         return (
-                            <div key={card.id} className="space-y-4 border-[1px] border-black/30 
+                            <motion.div 
+                                variants={SlideUp(card.delay)}
+                                initial="initial"
+                                whileInView={"animate"}
+                                key={card.id} 
+                                className="space-y-4 border-[1px] border-black/30 
                                 p-6 hover:bg-black hover:text-white hover:shadow-[7px_7px_0px_0px_#6c6c6c] duration-300"
                             >
                                 <span className="inline-block text-xl border-[1px] border-black rounded-full hover:border-white hover:bg-white hover:text-black p-4">{card.icon}</span>
                                 <p className="text-2xl font-bold font-serif">{card.title}</p>
                                 <p className="text-gray-400 text-xs">{card.description}</p>
                                 <a href={card.link} className="inline-block border-b border-black">Learn More</a>
-                            </div>
+                            </motion.div>
                         )
                     })
                 }
