803f38a57dfbd47d66fa8bc92df18ce60ace0a86
wacho91 <criskol.71@gmail.com>
2024-09-17 11:04:57 -0500

    Animation Banner2

--- src/Component/Banner/Banner2.jsx
+++ src/Component/Banner/Banner2.jsx
@@ -1,4 +1,6 @@
 
+import { motion } from "framer-motion"
+import { SlideUp } from "../../Animation/Animate"
 import Banner1 from "../../assets/banner2.png"
 
 const Banner2 = () => {
@@ -7,13 +9,31 @@ const Banner2 = () => {
         <div className="container py-14">
             <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
                 {/*Text Section*/}
-                <div className="space-y-5 flex justify-center flex-col">
-                    <h1 className="text-4xl font-bold font-serif">Simple way to make stylish living room</h1>
-                    <p className="text-gray-500 text-sm leading-7">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit
+                <div className="space-y-5 flex justify-center flex-col xl:max-w-[500px]">
+                    <motion.h1
+                        variants={SlideUp(0.2)}
+                        initial="initial"
+                        whileInView="animate" 
+                        className="text-4xl font-bold font-serif"
+                    >
+                        Simple way to make stylish living room
+                    </motion.h1>
+                    <motion.p 
+                        variants={SlideUp(0.4)}
+                        initial="initial"
+                        whileInView="animate"
+                        className="text-gray-500 text-sm leading-7"
+                    >
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit
                         earum accusantium tempore nam adipisicing elit. Suscipit earum
                         accusantium tempore nam aliquid{" "}
-                    </p>
-                    <div className="flex gap-3">
+                    </motion.p>
+                    <motion.div
+                        variants={SlideUp(0.6)}
+                        initial="initial"
+                        whileInView="animate" 
+                        className="flex gap-3"
+                    >
                         <div className="max-w-[80px] space-y-2">
                             <p className="text-3xl font-bold font-serif">15</p>
                             <p className="text-gray-500 text-sm">Years of Experience</p>
@@ -26,18 +46,28 @@ const Banner2 = () => {
                             <p className="text-3xl font-bold font-serif">34</p>
                             <p className="text-gray-500 text-sm">Award Gained</p>
                         </div>
-                    </div>
+                    </motion.div>
                     <div>
-                        <button className="primary-btn bg-black text-white hover:bg-white
-                         hover:text-black shadow-[5px_5px_0px_0px_#6c6c6c] hover:shadow-[5px_5px_0px_0px_#090808]"
+                        <motion.button 
+                            variants={SlideUp(0.6)}
+                            initial="initial"
+                            whileInView="animate" 
+                            className="primary-btn bg-black text-white hover:bg-white
+                          hover:text-black shadow-[5px_5px_0px_0px_#6c6c6c] hover:shadow-[5px_5px_0px_0px_#090808]"
                         >   
                             Contact Us
-                        </button>
+                        </motion.button>
                     </div>
                 </div>
                 {/*Image Section*/}
                 <div className="flex flex-col justify-center items-center">
-                    <img src={Banner1} alt=""  className="w-[95%] md:w-full mx-auto"/>
+                    <motion.img 
+                        initial={{ x: 100, opacity: 0 }}
+                        whileInView={{ x: 0, opacity: 1 }}
+                        transition={{ duration: 0.5, delay: 0.5 }}
+                        src={Banner1} 
+                        alt=""  
+                        className="w-[95%] md:w-full mx-auto"/>
                 </div>
             </div>
         </div>
