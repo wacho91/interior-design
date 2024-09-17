4e5af6cb661f7ec7c2c496208a20ef907ce7ffe9
wacho91 <criskol.71@gmail.com>
2024-09-17 11:17:23 -0500

    Animation NewsLetter

--- src/Component/NewsLetter/NewsLetter.jsx
+++ src/Component/NewsLetter/NewsLetter.jsx
@@ -1,19 +1,37 @@
-
+import { motion } from "framer-motion"
+import { SlideUp } from "../../Animation/Animate"
 
 const NewsLetter = () => {
   return (
     <div className='max-w-[500px] mx-auto space-y-5 py-14'>
-      <h1 className='text-3xl font-bold font-serif text-center'>Subscribe to our Newsletter</h1>
-      <p className='max-w-[400px] mx-auto text-gray-500 text-sm text-center'>
+      <motion.h1 
+        variants={SlideUp(0.2)}
+        initial="initial"
+        whileInView="animate"
+        className='text-3xl font-bold font-serif text-center'
+      >
+        Subscribe to our Newsletter
+      </motion.h1>
+      <motion.p 
+        variants={SlideUp(0.4)}
+        initial="initial"
+        whileInView="animate"
+        className='max-w-[400px] mx-auto text-gray-500 text-sm text-center'
+      >
         Lorem ipsum dolor sit amet consectetur 
         dipisicing elit. Ullam dignissimos eos 
         consequuntur, illum necessitatibus in.
-      </p>
+      </motion.p>
       {/*Form Here*/}
-      <div className='!mt-10 flex justify-center'>
+      <motion.div 
+        variants={SlideUp(0.6)}
+        initial="initial"
+        whileInView="animate"
+        className='!mt-10 flex justify-center'
+      >
         <input type="text" placeholder='Enter Your Email' className='px-4 py-4 border-[1px] border-black'/>
         <button className='bg-black text-white px-6 py-4 uppercase'>Subscribe</button>
-      </div>
+      </motion.div>
     </div>
   )
 }
