f7cda05b3cb392cd9c96f6fe007fd59881f411ce
wacho91 <criskol.71@gmail.com>
2024-09-17 11:13:06 -0500

    Animation testimonial

--- src/Component/Testimonial/Testimonial.jsx
+++ src/Component/Testimonial/Testimonial.jsx
@@ -1,4 +1,5 @@
-
+import { delay, motion } from "framer-motion";
+import { SlideLeft, SlideUp } from "../../Animation/Animate";
 
 const TestimonialData = [
     {
@@ -32,20 +33,37 @@ const Testimonial = () => {
     <div className="py-14">
         {/*Header Text*/}
         <div className="space-y-4 text-center max-w-[550px] mx-auto mb-8">
-            <h1 className="text-4xl font-bold font-serif">
+            <motion.h1 
+                variants={SlideUp(0.2)}
+                initial="initial"
+                whileInView="animate" 
+                className="text-4xl font-bold font-serif"
+            >
                 Words from our coustomers
-            </h1>
-            <p className="text-gray-500 text-sm max-w-[350px] mx-auto">
+            </motion.h1>
+            <motion.p 
+                variants={SlideUp(0.4)}
+                initial="initial"
+                whileInView="animate"
+                className="text-gray-500 text-sm max-w-[350px] mx-auto"
+            >
                 Bring your dream home to life with one-on-one design help & hand
                 picked products.
-            </p>
+            </motion.p>
         </div>
         {/* tesitomonial cards */}
         <div className="bg-black p-12">
             <div className="container grid grid-cols-1 md:grid-cols-3 gap-6">
                 {TestimonialData.map((card) => {
                     return (
-                        <div key={card.id} className="border-[1px] border-gray-500 p-5 text-white group hover:bg-white duration-300  cursor-pointer">
+                        <motion.div 
+                            variants={SlideLeft(card.delay)}
+                            initial="initial"
+                            whileInView="animate"
+                            key={card.id} 
+                            className="border-[1px] border-gray-500 p-5 text-white group 
+                            hover:bg-white duration-300  cursor-pointer"
+                        >
                             {/*Upper Section*/}
                             <div className="flex flex-row items-center gap-3">
                                 <img src={card.img} alt="image" className="w-[60px] rounded-full"/>
@@ -59,7 +77,7 @@ const Testimonial = () => {
                             <div className="mt-5 border-t-2 border-gray-500/40 pt-5">
                                 <p className="text-sm text-gray-300 group-hover:text-black duration-300">{card.text}</p>
                             </div>
-                        </div>
+                        </motion.div>
                     )
                 })}
             </div>
